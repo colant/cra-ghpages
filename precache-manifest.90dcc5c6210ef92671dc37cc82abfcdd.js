79b7138b82fe5a887d1b1639dc0deeb2dee3d731
colant <bin12vlxd@gmail.com>
2019-11-30 21:20:07 +0700

    Updates

---
+++ precache-manifest.90dcc5c6210ef92671dc37cc82abfcdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ebe3cfc1ea8acf4a86e0af9e329ce2",
+    "url": "/cra-ghpages/index.html"
+  },
+  {
+    "revision": "97cb4a01b02d4e5206c8",
+    "url": "/cra-ghpages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "90fd4461b4dc33195151",
+    "url": "/cra-ghpages/static/js/2.a6308b5e.chunk.js"
+  },
+  {
+    "revision": "97cb4a01b02d4e5206c8",
+    "url": "/cra-ghpages/static/js/main.0bc1b28a.chunk.js"
+  },
+  {
+    "revision": "7af5ef233a53c69b0b83",
+    "url": "/cra-ghpages/static/js/runtime-main.a01d41c3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/cra-ghpages/static/media/logo.25bf045c.svg"
+  }
+]);
